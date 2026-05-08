f6f29de147e8588c488f6773aedf5825a0fc342a
vincentboutin-dev <74202475+vincentboutin-dev@users.noreply.github.com>
2026-05-08 14:48:45 +0200

    chore: update state backend resource group to rg-jf-tfstate-frc

--- JobFinder/Terraform/envs/lz_prod/backend.tf
+++ JobFinder/Terraform/envs/lz_prod/backend.tf
@@ -2,7 +2,7 @@
 # Replace CHANGE_ME with the actual storage account name before running terraform init.
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
+    resource_group_name  = "rg-jf-tfstate-frc"
     storage_account_name = "stjftfstatefrc" # Template users: replace with your own Terraform state storage account name
     container_name       = "tfstate"
     key                  = "lz-prod.tfstate"
