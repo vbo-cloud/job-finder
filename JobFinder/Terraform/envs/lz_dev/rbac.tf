d022537d1f703dce880218796e7d28bc8e17be2c
vincentboutin-dev <74202475+vincentboutin-dev@users.noreply.github.com>
2026-05-26 20:06:36 +0200

    style(lz): reorder locals — scopes map before condition string

--- JobFinder/Terraform/envs/lz_dev/rbac.tf
+++ JobFinder/Terraform/envs/lz_dev/rbac.tf
@@ -83,13 +83,13 @@ resource "azurerm_role_assignment" "sp_github" {
 # Same condition as sp-jf-platform: cannot assign Owner, User Access Administrator,
 # or Role Based Access Control Administrator.
 locals {
-  rbac_admin_condition = "((!(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})) OR (@Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168})) AND ((!(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})) OR (@Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}))"
-
   sp_github_rbac_admin_scopes = {
     rg_core = module.rg_core.id
     rg_app  = module.rg_app.id
     rg_data = module.rg_data.id
   }
+
+  rbac_admin_condition = "((!(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})) OR (@Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168})) AND ((!(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})) OR (@Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}))"
 }
 
 resource "azurerm_role_assignment" "sp_github_rbac_admin" {
