fce848c95b54d5d7db19192efbe3ef324b64f9fb
vincentboutin-dev <74202475+vincentboutin-dev@users.noreply.github.com>
2026-05-26 19:53:14 +0200

    feat(lz): grant conditioned RBAC Administrator to sp-jf-github on dev resource groups

--- JobFinder/Terraform/envs/lz_dev/rbac.tf
+++ JobFinder/Terraform/envs/lz_dev/rbac.tf
@@ -76,3 +76,27 @@ resource "azurerm_role_assignment" "sp_github" {
   role_definition_name = each.value.role_definition_name
   principal_id         = var.sp_github_object_id
 }
+
+# RBAC Administrator (conditioned) scoped to each dev resource group.
+# Allows sp-jf-github to create role assignments within dev/ (e.g. AcrPull
+# for Container App Jobs) without requiring sp-jf-platform for app-level RBAC.
+# Same condition as sp-jf-platform: cannot assign Owner, User Access Administrator,
+# or Role Based Access Control Administrator.
+locals {
+  rbac_admin_condition = "((!(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})) OR (@Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168})) AND ((!(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})) OR (@Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAllValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}))"
+
+  sp_github_rbac_admin_scopes = {
+    rg_core = module.rg_core.id
+    rg_app  = module.rg_app.id
+    rg_data = module.rg_data.id
+  }
+}
+
+resource "azurerm_role_assignment" "sp_github_rbac_admin" {
+  for_each             = local.sp_github_rbac_admin_scopes
+  scope                = each.value
+  role_definition_name = "Role Based Access Control Administrator"
+  principal_id         = var.sp_github_object_id
+  condition_version    = "2.0"
+  condition            = local.rbac_admin_condition
+}
